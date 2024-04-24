4033d0bdd4a343072bba5f909b6f05e66fa1af2e
Ertzy <hertzgaard@gmail.com>
2024-04-24 11:04:13 +0200

    Node auth

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const authRoutes = require('./routes/authRoutes')
+const cookieParser = require('cookie-parser');
+const { requireAuth, checkUser } = require("./middelware/authMiddelware");
+
+const app = express();
+
+app.use(express.static('public'));
+app.use(express.json());
+app.use(cookieParser());
+
+app.set("view engine", "ejs");
+
+const dbURL = "mongodb://10.12.8.45:27017/?directConnection=true&appName=mongosh+2.1.5"
+mongoose.connect(dbURL, {})
+    .then((result) => app.listen(3000))
+    .catch((err) => console.log(err));
+
+
+
+// routes
+app.get('*', checkUser);
+app.get('/', (req, res) => res.render('index'));
+app.get('/smoothies', requireAuth, (req, res) => res.render('smoothies'));
+app.use(authRoutes)
